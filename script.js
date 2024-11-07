5bc1c78062be8d39a486aefec3f92ff18befd0a8
dadaiii <elisa.lane06@gmail.com>
2024-11-07 23:49:28 +0100

    Intégration vidéo player

--- script.js
+++ script.js
@@ -16,46 +16,39 @@ document.title = randomTitle;
 // Sélectionne tous les éléments ayant la classe "folder" dans la sidebar
 document.querySelectorAll('.sidebar .folder').forEach(folder => {
     folder.addEventListener('click', function(event) {
-        // Empêche la propagation du clic aux éléments parents
         event.stopPropagation();
 
-        // Sélectionne le sous-dossier (ul) suivant le dossier cliqué
         const subFolder = this.querySelector('ul');
         if (subFolder) {
-            // Change l'état d'affichage du sous-dossier (affiche ou masque)
             subFolder.style.display = subFolder.style.display === 'block' ? 'none' : 'block';
         }
 
-        // Si le dossier cliqué est "Le film", afficher le lecteur vidéo
         if (this.id && this.id.startsWith('le-film')) {
             const videoContent = document.getElementById('video-content');
             const videoPlayer = document.getElementById('film-player');
-            let videoUrl = ""; // Variable pour l'URL de la vidéo
+            let videoUrl = ""; 
 
-            // Définir l'URL de la vidéo YouTube en fonction du dossier
             switch(this.id) {
                 case "le-film-feutre":
-                    videoUrl = "https://www.youtube.com/embed/D2OSE5Fnw7g"; // Remplacez par l'ID réel de YouTube
+                    videoUrl = "https://www.youtube.com/embed/MvvCZeqBebY"; 
                     break;
                 case "le-film-fin":
-                    videoUrl = "https://www.youtube.com/embed/D2OSE5Fnw7g"; // ID de la vidéo "La fin"
+                    videoUrl = "https://www.youtube.com/embed/D2OSE5Fnw7g"; 
                     break;
                 case "le-film-seanmhathair":
-                    videoUrl = "https://www.youtube.com/embed/ID_SEANMHATHAIR"; // Remplacez par l'ID réel de YouTube
+                    videoUrl = "https://www.youtube.com/embed/bKtLhx3766s"; 
                     break;
                 case "le-film-maison-poupees":
-                    videoUrl = "https://www.youtube.com/embed/ID_MAISON_POUPEES"; // Remplacez par l'ID réel de YouTube
+                    videoUrl = "https://www.youtube.com/embed/ID_MAISON_POUPEES"; 
                     break;
                 case "le-film-camion":
-                    videoUrl = "https://www.youtube.com/embed/ID_CAMION"; // Remplacez par l'ID réel de YouTube
+                    videoUrl = "https://www.youtube.com/embed/ID_CAMION"; 
                     break;
             }
 
-            // Mettre à jour la source de la vidéo
             videoPlayer.src = videoUrl;
-
-            // Afficher le contenu vidéo
-            videoContent.style.display = 'block'; // Rendre visible le lecteur vidéo
+            videoContent.style.display = 'block'; // Afficher le lecteur vidéo
         }
     });
 });
+
