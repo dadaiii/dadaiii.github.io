d3e1c70b56d69d9e21d08e3391bba3e3273aedde
dadaiii <elisa.lane06@gmail.com>
2025-05-02 13:55:13 +0200

    sean carnet

--- script.js
+++ script.js
@@ -35,3 +35,100 @@ window.addEventListener('DOMContentLoaded', () => {
     // Affiche le message dans la section de bienvenue
     document.getElementById("welcome-text").querySelector("p").textContent = randomMessage;
 });
+
+
+// Carrousel d'images pour souvenirs de la pierre
+let currentIndex = 0;
+
+// Définissez les images uniquement ici
+const images = [
+    "https://imgur.com/it4jsei.jpg",
+    "https://imgur.com/TOy635P.jpg",
+    "https://imgur.com/PVYyIvX.jpg",
+    "https://imgur.com/Wd0zgdn.jpg",
+    "https://imgur.com/FQ2IbmG.jpg",
+    "https://imgur.com/KDunwJf.jpg",
+    "https://imgur.com/FRsPyl8.jpg",
+    "https://imgur.com/6PGPRRW.jpg",
+    "https://imgur.com/LQuXTYW.jpg",
+    "https://imgur.com/gRbP12j.jpg",
+    "https://imgur.com/ileEkmp.jpg",
+    "https://imgur.com/g9eDNtr.jpg",
+    "https://imgur.com/yLyt99R.jpg",
+    "https://imgur.com/y8b1sD7.jpg",
+    "https://imgur.com/9pYEHHu.jpg",
+    "https://imgur.com/DH9jNhy.jpg",
+    "https://imgur.com/DORZIgs.jpg",
+    "https://imgur.com/GSmFi9z.jpg",
+    "https://imgur.com/Y7CwFyF.jpg",
+    "https://imgur.com/rXpSsNC.jpg",
+    "https://imgur.com/ijEpP4H.jpg",
+    "https://imgur.com/BO0aIwj.jpg",
+    "https://imgur.com/DwFWKQU.jpg",
+    "https://imgur.com/K3dEghy.jpg",
+    "https://imgur.com/YNS8QkK.jpg",
+    "https://imgur.com/vSqSUZq.jpg",
+    "https://imgur.com/PrF9WTm.jpg",
+    "https://imgur.com/yL4sK5I.jpg",
+    "https://imgur.com/AHkRRh3.jpg",
+    "https://imgur.com/z48B40I.jpg",
+    "https://imgur.com/jnlpsXi.jpg",
+    "https://imgur.com/jZZzNZw.jpg",
+    "https://imgur.com/axTZWzF.jpg",
+    "https://imgur.com/jgLWhfq.jpg",
+    "https://imgur.com/QdxAnKh.jpg",
+    "https://imgur.com/zkJrLMX.jpg",
+    "https://imgur.com/YhJ1TDx.jpg",
+    "https://imgur.com/hgur9zK.jpg",
+    "https://imgur.com/94E2gOz.jpg",
+    "https://imgur.com/RyYRtHC.jpg",
+    "https://imgur.com/FGwaUyk.jpg",
+    "https://imgur.com/n3rcwKm.jpg",
+    "https://imgur.com/AJGpNAK.jpg",
+    "https://imgur.com/Jt4o0Ru.jpg",
+    "https://imgur.com/gJ5hJjK.jpg",
+    "https://imgur.com/4CQfYeh.jpg",
+    "https://imgur.com/un6EWP0.jpg",
+    "https://imgur.com/LMsAjQ1.jpg",
+    "https://imgur.com/sNMyihZ.jpg",
+    "https://imgur.com/VJrDrWU.jpg",
+    "https://imgur.com/lk4GnOY.jpg",
+    "https://imgur.com/PwY2XlO.jpg",
+    "https://imgur.com/E9ERNeH.jpg",
+    "https://imgur.com/yzy72aB.jpg",
+    "https://imgur.com/zJFsx1l.jpg"
+];
+
+// Fonction pour précharger les images
+function preloadImages(imageUrls) {
+    imageUrls.forEach((url) => {
+        const img = new Image();
+        img.src = url;
+    });
+}
+
+function updateCarousel() {
+    const carouselImages = document.querySelector(".carousel-images");
+    carouselImages.innerHTML = `
+        <img src="${images[currentIndex]}" alt="Image du projet Seanmathair" onclick="openLightbox('${images[currentIndex]}')">
+        <img src="${images[(currentIndex + 1) % images.length]}" alt="Image du projet Seanmathair" onclick="openLightbox('${images[(currentIndex + 1) % images.length]}')">
+    `;
+}
+
+function prevImages(event) {
+    if (event) event.preventDefault(); // Empêche le comportement par défaut
+    currentIndex = (currentIndex - 2 + images.length) % images.length;
+    updateCarousel();
+}
+
+function nextImages(event) {
+    if (event) event.preventDefault(); // Empêche le comportement par défaut
+    currentIndex = (currentIndex + 2) % images.length;
+    updateCarousel();
+}
+
+// Initialisation du carrousel
+document.addEventListener("DOMContentLoaded", () => {
+    preloadImages(images); // Précharge toutes les images
+    updateCarousel(); // Initialise le carrousel
+});
